869c54980c4373161b0e9eb3959fbd60e8df406f
yqp <468417415@qq.com>
2020-05-18 10:53:51 +0800

    finished most

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,10 @@ import logo from "../images/logo.jpg"
 
 export default class Header extends Component {
     render(){
+        const {scrollToAnchor} = this.props;
         return(
             <Navbar bg="light" expand="lg"  sticky="top" className='header'>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand className='navbtn navlogo' onClick={()=>scrollToAnchor('home')} >
                 <img
                     alt=""
                     src={logo}
@@ -14,14 +15,15 @@ export default class Header extends Component {
                     height="50"
                     className="d-inline-block align-top"
                 />
+                <div className='title-font' >邦方生物</div>
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <Nav.Link href="#home" className='navbtn'>首页</Nav.Link>
-                    <Nav.Link href="#prdcenter" className='navbtn'>产品中心</Nav.Link>
-                    <Nav.Link href="#prdintro" className='navbtn'>产品介绍</Nav.Link>
-                    <Nav.Link href="#aboutus">关于我们</Nav.Link>
-                    <Nav.Link href="#culture">企业文化</Nav.Link>
+                    <Nav.Link  className='navbtn' onClick={()=>scrollToAnchor('home')}>首页</Nav.Link>
+                    <Nav.Link  className='navbtn'  onClick={()=>scrollToAnchor('prdcenter')}>产品中心</Nav.Link>
+                    <Nav.Link  className='navbtn' onClick={()=>scrollToAnchor('prdintro')}>产品介绍</Nav.Link>
+                    <Nav.Link className='navbtn' onClick={()=>scrollToAnchor('aboutus')}>关于我们</Nav.Link>
+                    <Nav.Link className='navbtn' onClick={()=>scrollToAnchor('culture')}>企业文化</Nav.Link>
                     
                 </Navbar.Collapse>
                 
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -12,10 +12,19 @@ import Culture from '../../components/Culture'
 
 
 class HomePage extends Component {
+  scrollToAnchor = (anchorName) => {
+    if (anchorName) {
+        // 找到锚点
+        console.log(anchorName)
+        let anchorElement = document.getElementById(anchorName);
+        // 如果对应id的锚点存在，就跳转到锚点
+        if(anchorElement) { anchorElement.scrollIntoView({block: 'start', behavior: 'smooth'}); }
+    }
+  }
   render() {
     return (
       <div>
-        <Header/>
+        <Header scrollToAnchor={this.scrollToAnchor}/>
         <ControlledCarousel/>
         <ProductCenter/>
         <ProductIntroduce/>
